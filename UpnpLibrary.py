e139682d806f60c32710390208abad1b36dd7dfe
Lionel Ains <lionel.ains@legrand.fr>
2015-09-08 16:16:49 +0200

    Initial import

---
+++ UpnpLibrary.py
@@ -0,0 +1,237 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" Legrand MP5B """
+
+from __future__ import division
+
+import Queue
+import re
+import time
+import urllib
+
+from upnp_http import UpnpHttp
+from upnp_socket import UpnpSocket
+from upnp_thread import UpnpThread
+
+from common import LegrandError
+from ToolLibrary import ToolLibrary
+
+
+class UpnpLibrary:
+
+    """ robotframework UPnP library """
+
+    ROBOT_LIBRARY_DOC_FORMAT = 'ROBOT'
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+    ROBOT_LIBRARY_VERSION = '1.0'
+
+    BURST = 70
+    POLL_WAIT = 1 / 100
+
+    def __init__(self):
+
+        self._upnp_socket = UpnpSocket()
+        self._upnp_socket.init_sockets()
+        self._queue = Queue.Queue()
+        self._upnp_thread = None
+        self._upnp_http = UpnpHttp()
+
+    def start(self):
+        """ Start UPnP thread.
+
+        Example:
+        | Start |
+        """
+
+        if not self._upnp_thread:
+            self._upnp_thread = UpnpThread(self._upnp_socket, self._queue)
+        if not self._upnp_thread.is_alive():
+            try:
+                self._upnp_thread.start()
+            except StandardError:
+                raise LegrandError('Thread already started')
+
+    def stop(self):
+        """ Stop UPnP thread.
+
+        Example:
+        | Stop |
+        """
+
+        if self._upnp_thread:
+            if self._upnp_thread.isAlive():
+                self._upnp_thread.stop()
+                try:
+                    self._upnp_thread.join()
+                except RuntimeError:
+                    raise LegrandError('Join deadlock')
+                self._upnp_thread = None
+
+    def get_ip(self, mac):
+        """ Get first IP address which have `mac` in UUID.
+
+        Return IP.
+
+        Example:
+        | Get IP | 01.23.45.67.89.ab |
+        =>
+        | ${IP} |
+        """
+
+        ret = ''
+        self._upnp_thread.clear()
+        wait_test = {'start': self._upnp_http.generate_start_notify(eol=False)}
+        mac = ToolLibrary.mac_string(mac)
+        maxtime = time.time() + UpnpLibrary.BURST
+        while True:
+            time.sleep(UpnpLibrary.POLL_WAIT)
+            if time.time() > maxtime:
+                raise LegrandError('Expected one NOTIFY/alive')
+            temp = self._upnp_thread.wait(wait_test, address=None, timeout=UpnpLibrary.BURST)
+            if temp is not None:
+                (data, addr) = temp
+                result = re.match('^uuid:(.*):{2}(.*)$', data['USN'])
+                if result is not None:
+                    uuid = result.groups()[0]
+                    uuid_mac = ToolLibrary.mac_string(uuid.split('-')[-1])
+                    if uuid_mac == mac:
+                        ret = addr[0]
+                        break
+        ret = unicode(ret)
+        return ret
+
+    def clear_queue(self):
+        """ Delete all UPnP packet received.
+
+        Example:
+        | Clear Queue |
+        """
+
+        self._upnp_thread.clear()
+
+    def check_on_to_off(self, addr):
+        """ Wait a UPnP NOTIFY/byebye on `addr` until BURST time.
+        The queue has to be reset manually.
+
+        Return data
+
+        Example:
+        | Clear Queue |
+        | Check On To Off | ip |
+        =>
+        | ${data} |
+        """
+
+        wait_test = {'start': self._upnp_http.generate_start_notify(eol=False), 'NTS': 'ssdp:byebye'}
+        data = self._upnp_thread.wait(wait_test, address=addr, timeout=UpnpLibrary.BURST)
+        if not data:
+            raise LegrandError('Expected one NOTIFY/byebye')
+        return data
+
+    def check_on(self, addr):
+        """ Send a msearch on `addr` and wait response until BURST time.
+
+        Return data
+
+        Example:
+        | Check On | ip |
+        =>
+        | ${data} |
+        """
+
+        self._upnp_thread.clear()
+        request = self._upnp_http.generate_search_request_multicast('upnp:rootdevice')
+        self._upnp_socket.send_request(request)
+        wait_test = {'start': self._upnp_http.generate_start_response(eol=False)}
+        data = self._upnp_thread.wait(wait_test, address=addr, timeout=UpnpLibrary.BURST)
+        if not data:
+            raise LegrandError('Expected one response')
+        return data
+
+    def check_run(self, addr):
+        """ Wait a UPnP NOTIFY/alive on `addr` until BURST time.
+
+        Return data
+
+        Example:
+        | Check Run | ip | 
+        =>
+        | ${data} |
+        """
+
+        self._upnp_thread.clear()
+        wait_test = {'start': self._upnp_http.generate_start_notify(eol=False), 'NTS': 'ssdp:alive'}
+        data = self._upnp_thread.wait(wait_test, address=addr, timeout=UpnpLibrary.BURST)
+        if not data:
+            raise LegrandError('Expected one NOTIFY/alive')
+        return data
+
+    def check_stop(self, addr):
+        """ Wait no UPnP NOTIFY/alive on `addr` until BURST time.
+
+        Example:
+        | Check Stop | ip |
+        """
+
+        self._upnp_thread.clear()
+        wait_test = {'start': self._upnp_http.generate_start_notify(eol=False), 'NTS': 'ssdp:alive'}
+        data = self._upnp_thread.wait(wait_test, address=addr, timeout=UpnpLibrary.BURST)
+        if data:
+            raise LegrandError('Expected no NOTIFY')
+
+    @staticmethod
+    def retrieve_xml(data):
+        """ Retrieve XML file from `data`.
+
+        Return filename.
+
+        Example:
+        | ${data} = | Check Run | ip |
+        | Retrieve Xml | ${data} | 
+        =>
+        | ${filename} |
+        """
+
+        ret = ''
+        try:
+            data_parsed = data[0]
+            url = data_parsed['LOCATION']
+        except StandardError:
+            raise LegrandError('No location')
+        try:
+            ret = urllib.urlretrieve(url)[0]
+        except StandardError:
+            raise LegrandError('Unable to retrieve xml')
+        ret = unicode(ret)
+        return ret
+
+
+if __name__ == '__main__':
+    from console_logger import LOGGER as logger
+
+    try:
+        input = raw_input
+    except NameError:
+        pass
+
+    MAC = '00:04:74:05:00:f0'
+    UL = UpnpLibrary()
+    try:
+        UL.start()
+        input('Press enter & "Enable UPnP/Bonjour" on web interface')
+        IP = UL.get_ip(MAC)
+        assert IP == '10.10.8.39'
+        input('Press enter & "Disable UPnP/Bonjour" on web interface')
+        UL.clear_queue()
+        DATA = UL.check_on_to_off(IP)
+        DATA = UL.check_stop(IP)
+        input('Press enter & "Enable UPnP/Bonjour" on web interface')
+        DATA = UL.check_on(IP)
+        DATA = UL.check_run(IP)
+        FILENAME = UL.retrieve_xml(DATA)
+        assert FILENAME is not None
+    finally:
+        UL.stop()
+else:
+    from robot.api import logger
---
+++ upnp_http.py
@@ -0,0 +1,243 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" Legrand MP5B """
+
+from common import LegrandError
+
+
+class UpnpHttp:
+
+    """ generate and parse SSDP UPnP HTTP messages """
+
+    SEP = ':'
+    EOL = '\r\n'
+    ALL = '*'
+    SPACE = ' '
+    SSDP_MULTICAST = '239.255.255.250:1900'
+    VERSION = 'HTTP/1.1'
+    NO_ERROR = '200 OK'
+
+    def __init__(self, version=None):
+        if not version:
+            self.version = UpnpHttp.VERSION
+        else:
+            self.version = version
+
+    def generate_start_notify(self, eol=True):
+        """ generate start notify """
+
+        temp = UpnpHttp.SPACE.join(['NOTIFY', UpnpHttp.ALL, self.version])
+        if eol:
+            temp += UpnpHttp.EOL
+        return temp
+
+    def generate_start_search(self, eol=True):
+        """ generate start search """
+
+        temp = UpnpHttp.SPACE.join(['M-SEARCH', UpnpHttp.ALL, self.version])
+        if eol:
+            temp += UpnpHttp.EOL
+        return temp
+
+    def generate_start_response(self, status_code=None, eol=True):
+        """ generate start response """
+
+        if status_code is None:
+            status_code = UpnpHttp.NO_ERROR
+        temp = UpnpHttp.SPACE.join([self.version, status_code])
+        if eol:
+            temp += UpnpHttp.EOL
+        return temp
+
+    def generate_start_get(self, description_path, eol=True):
+        """ generate start get """
+
+        temp = UpnpHttp.SPACE.join(['GET', description_path, self.version])
+        if eol:
+            temp += UpnpHttp.EOL
+        return temp
+
+    @staticmethod
+    def check_intrange(data, mini, maxi):
+        """ verify data is an int between mini and maxi """
+
+        try:
+            temp = int(data)
+        except StandardError:
+            raise LegrandError('Value %s is not an integer' % data)
+        if temp < mini or temp > maxi:
+            raise LegrandError('Integer %s is not in [%d;%d] range' % (data, mini, maxi))
+        return temp
+
+    @staticmethod
+    def generate_header(name, value, eol=True):
+        """ generate a standard header """
+
+        temp = str(name).upper() + UpnpHttp.SEP + str(value)
+        if eol:
+            temp += UpnpHttp.EOL
+        return temp
+
+    def generate_device_available(
+        self,
+        url,
+        notification_type,
+        server,
+        uniq_service_name,
+        max_age=120,
+        ):
+        """ generate device available, i.e NOTIFY/alive"""
+
+        start = self.generate_start_notify(True)
+        headers = UpnpHttp.generate_header('HOST', UpnpHttp.SSDP_MULTICAST)
+        headers += UpnpHttp.generate_header('CACHE-CONTROL', 'max-age = %d' % UpnpHttp.check_intrange(max_age, 1, 1800))
+        headers += UpnpHttp.generate_header('LOCATION', url)
+        headers += UpnpHttp.generate_header('NT', notification_type)
+        headers += UpnpHttp.generate_header('NTS', 'ssdp:alive')
+        headers += UpnpHttp.generate_header('SERVER', server)
+        headers += UpnpHttp.generate_header('USN', uniq_service_name)
+        request = start + headers + UpnpHttp.EOL
+        return request
+
+    def generate_device_unavailable(self, notification_type, uniq_service_name):
+        """ generate device unavailable, i.e NOTIFY/byebye"""
+
+        start = self.generate_start_notify(True)
+        headers = UpnpHttp.generate_header('HOST', UpnpHttp.SSDP_MULTICAST)
+        headers += UpnpHttp.generate_header('NT', notification_type)
+        headers += UpnpHttp.generate_header('NTS', 'ssdp:byebye')
+        headers += UpnpHttp.generate_header('USN', uniq_service_name)
+        request = start + headers + UpnpHttp.EOL
+        return request
+
+    def generate_device_update(
+        self,
+        url,
+        notification_type,
+        uniq_service_name,
+        ):
+        """ generate device update, i.e NOTIFY/update"""
+
+        start = self.generate_start_notify(True)
+        headers = UpnpHttp.generate_header('HOST', UpnpHttp.SSDP_MULTICAST)
+        headers += UpnpHttp.generate_header('LOCATION', url)
+        headers += UpnpHttp.generate_header('NT', notification_type)
+        headers += UpnpHttp.generate_header('NTS', 'ssdp:update')
+        headers += UpnpHttp.generate_header('USN', uniq_service_name)
+        request = start + headers + UpnpHttp.EOL
+        return request
+
+    def generate_search_request_multicast(
+        self,
+        search_target,
+        maximum_wait=2,
+        user_agent=None,
+        ):
+        """ generate search request, i.e M-SEARCH/multicast"""
+
+        start = self.generate_start_search()
+        headers = UpnpHttp.generate_header('HOST', UpnpHttp.SSDP_MULTICAST)
+        headers += UpnpHttp.generate_header('MAN', 'ssdp:discover')
+        headers += UpnpHttp.generate_header('MX', UpnpHttp.check_intrange(maximum_wait, 1, 5))
+        headers += UpnpHttp.generate_header('ST', search_target)
+        if user_agent is not None:
+            headers += UpnpHttp.generate_header('USER_AGENT', user_agent)
+        request = start + headers + UpnpHttp.EOL
+        return request
+
+    def generate_search_request_unicast(
+        self,
+        search_target,
+        hostname,
+        port_number,
+        user_agent=None,
+        ):
+        """ generate search request, i.e M-SEARCH/unicast"""
+
+        start = self.generate_start_search()
+        headers = UpnpHttp.generate_header('HOST', UpnpHttp.SEP.join([hostname, port_number]))
+        headers += UpnpHttp.generate_header('MAN', 'ssdp:discover')
+        headers += UpnpHttp.generate_header('ST', search_target)
+        if user_agent is not None:
+            headers += UpnpHttp.generate_header('USER_AGENT', user_agent)
+        request = start + headers + UpnpHttp.EOL
+        return request
+
+    def generate_search_response(
+        self,
+        url,
+        server,
+        search_target,
+        uniq_service_name,
+        date=None,
+        max_age=120,
+        ):
+        """ generate search response """
+
+        start = self.generate_start_response()
+        headers = UpnpHttp.generate_header('CACHE-CONTROL', 'max-age = %d' % UpnpHttp.check_intrange(max_age, 1, 1800))
+        if date is not None:
+            headers += UpnpHttp.generate_header('DATE', date)
+        headers += UpnpHttp.generate_header('EXT', '')
+        headers += UpnpHttp.generate_header('LOCATION', url)
+        headers += UpnpHttp.generate_header('SERVER', server)
+        headers += UpnpHttp.generate_header('ST', search_target)
+        headers += UpnpHttp.generate_header('USN', uniq_service_name)
+        request = start + headers + UpnpHttp.EOL
+        return request
+
+    def generate_get_description(
+        self,
+        description_path,
+        hostname,
+        port_number=80,
+        language=None,
+        ):
+        """ generate get description, i.e GET"""
+
+        start = self.generate_start_get(description_path)
+        headers = UpnpHttp.generate_header('HOST', UpnpHttp.SEP.join([hostname, port_number]))
+        if language is not None:
+            headers += UpnpHttp.generate_header('ACCEPT-LANGUAGE', language)
+        request = start + headers + UpnpHttp.EOL
+        return request
+
+    @staticmethod
+    def parse_data(data):
+        """ parse received data """
+
+        data_dict = {}
+        lines = data.split(UpnpHttp.EOL)
+        data_dict['start'] = lines[0]  # start key is used for the first line
+        for line in lines[1:]:
+            try:
+                (name, value) = line.split(UpnpHttp.SEP, 1)
+                name = name.upper()
+                name.strip()
+            except StandardError:
+                pass
+            else:
+                data_dict[name] = value.strip()
+        return data_dict
+
+    @staticmethod
+    def parse_location(url):
+        """ parse url """
+
+        host = False
+        page = False
+        try:
+            (method, temp) = url.split('://', 1)
+            (host, page) = temp.split('/', 1)
+        except StandardError:
+            page = url
+        return (method, host, page)
+
+
+if __name__ == '__main__':
+    UH = UpnpHttp('HTTP/1.0')
+    assert 'NOTIFY * HTTP/1.0' == UH.generate_start_notify(eol=False)
+    assert 'M-SEARCH * HTTP/1.0' == UH.generate_start_search(eol=False)
+    assert 'HTTP/1.0 200 OK' == UH.generate_start_response(eol=False)
+    assert 'GET /path HTTP/1.0' == UH.generate_start_get('/path', eol=False)
